95f61428fe83c205d17bcc2efc4ba43b565c7ba9
vitaliy <1111vitt77@gmail.com>
2017-08-21 16:13:24 +0300

    second page toppotolok

---
+++ page2/js/script.js
@@ -0,0 +1,56 @@
+$(function () {
+
+    $('header nav ul li:nth-child(3)').click(function() {
+            $('.ceil_types').toggleClass('open');
+    });
+
+
+    $('header .ceil_types a .ceil_types-item').click('on', function() {
+        $('.ceil_types').toggleClass('open');
+    });
+
+    $('header .menu_item:nth-child(3)').click('on', function() {
+        $('header .menu_item ul').css('display', 'block');
+    });
+
+
+
+    $("#slider").slider({
+        range: "min",
+        value: 32,
+        min: 1,
+        max: 50,
+        slide: function (event, ui) {
+            $("#amount").val(ui.value);
+        }
+    });
+
+    $("#amount").val($("#slider").slider("value"));
+
+    /* Menu toggle */
+
+    function menuToggle() {
+        if($('.menu').hasClass('open')){
+            $('header .menu_item ul').css('display', 'none');
+        }
+        $('.menu').toggleClass('open');
+        $('.menu_expand').toggleClass('open'); 
+    }
+
+    $('.menu').click(menuToggle);
+    /* $('.menu_item').click(menuToggle); */
+
+
+});
+    
+    function changecolor(arg) {
+        var el = arg.parentElement.parentElement.parentElement.parentElement;
+        var name = arg.value;
+        var src = 'img/lamps/';
+        el.children[0].style.backgroundImage = 'url(' + src + name + '.jpg)';
+    };
+
+    /* document.onclick = function() {
+        console.log('in x');
+        document.getElementById('ceil_types').style.display = 'none'
+    }  */
